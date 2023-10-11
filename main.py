89a66b3a8cf3feec638007c32e46ee9fa8896591
Hossein Molavi <75816912+hmolavi@users.noreply.github.com>
2023-10-11 11:19:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
 predict = "G3"
 
-X = np.array(data.drop([predict], 1))
+x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
 linear = linear_model.LinearRegression()
 
